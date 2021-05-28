a571747e7c931e2eb19502b3ed5f1f73728b2b45
Sachin Gupta <sachin070gupta@gmail.com>
2021-05-28 11:21:28 +0530

    added router and fetching data from DB

--- src/main/java/com/ipl/dashboard/controller/TeamController.java
+++ src/main/java/com/ipl/dashboard/controller/TeamController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.ipl.dashboard.repository.MatchRepository;
 import com.ipl.dashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 	
 	private TeamRepository tRepo;
